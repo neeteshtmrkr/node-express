e457ad7dbbeb926fbc0f8a82afad8c62f4518e91
neetsh24 <tamrakarnitesh21@gmail.com>
2020-06-04 12:01:11 +0545

    Assignment 1: Node Modules, Express and REST API

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -26,8 +26,36 @@ dishRouter.route('/')
     res.end('Put operation not supported on /dishes');
 })
 
+
 .delete((req,res,next)=>{
     res.end('Deleting all the dishes!');
+});
+
+dishRouter.route('/:dishId')
+.get((req, res, next) => {
+    res.end('Will Send details of the dish:'
+        +req.params.dishId + 'to you');
+
+})
+
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /dishes/'
+        +req.params.dishId);
+   
+
+})
+
+.put((req, res, next) => {
+    statusCode = 403;
+    res.write('Updation the dish:' +req.params.dishId + '\n');
+    res.end('Will update the dish: ' +req.body.name + 'with details' +req.body.description)
+
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' +req.params.dishId);
+
 });
 
 module.exports=dishRouter;
